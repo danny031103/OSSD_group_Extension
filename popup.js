202f9fd05ff010e689ba6b825132b6d99a1d5c9c
danny031103 <db4471@nyu.edu>
2025-02-09 13:57:10 -0500

    added commands to list

--- popup.js
+++ popup.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Popup script loaded!");
 
-    const commands = [
-        "git init - Initialize a new repo",
-        "git clone <url> - Clone a repo",
+    const commands=[
+        "git init - Initialize a new Repo",
+        "git clone <url> - Clone a Repo",
         "git status - Check changes",
         "git add . - Stage all changes",
-        "git commit -m 'message' - Commit changes",
+        "git commit -m 'MESSAGE' - Commit changes",
         "git push - Push commits",
-        "git pull - Pull latest changes"
+        "git pull - Pull latest changes",
+        "git branch - List branches",
+        "git branch <name> - Create a new branch",
+        "git checkout <branch> - Switch to a branch",
+        "git checkout -b <branch> - Create and switch to a branch",
+        "git merge <branch> - Merge a branch into the current branch",
+        "git stash - Temporarily save changes",
     ];
-
-    const list = document.createElement("ul");
-    
+    const list=document.createElement("ul");
     commands.forEach(cmd => {
-        const li = document.createElement("li");
-        li.innerHTML = `<code>${cmd}</code>`;
+        const li=document.createElement("li");
+        li.innerHTML=`<code>${cmd}</code>`;
         list.appendChild(li);
     });
-
     document.body.appendChild(list);
 });
